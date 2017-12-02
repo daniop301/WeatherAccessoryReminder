735be8865399288baad44c8d7a2cab83e62ead83
daniop301 <danielpytlos@gmail.com>
2017-12-02 19:16:54 +0100

    Added weka model

--- app/src/main/java/ca/group3/weatheraccessoryreminder/MainActivity.java
+++ app/src/main/java/ca/group3/weatheraccessoryreminder/MainActivity.java
@@ -17,14 +17,28 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.trees.J48;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+import weka.core.SparseInstance;
+
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -36,12 +50,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Spinner sItems;
     FileOutputStream outputStream;
     private boolean outputStreamFlag = false;
+    private ArrayList<Double> window1;
+    private ArrayList<Double> window2;
+    private TextView tvData;
+    int counter = 0;
+    private long lastSensorUpdate = 0;
+    private int counter2 = 0;
+    private J48 classifier;
+    private FastVector fvWekaAttributes;
+    private Instance inst_co;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         isStoragePermissionGranted();
+        window1 = new ArrayList<Double>();
+        window2 = new ArrayList<Double>();
+        tvData = findViewById(R.id.textViewData);
+        createAttributes();
+
+        try {
+            File sdcard = Environment.getExternalStorageDirectory();
+            classifier = (J48) weka.core.SerializationHelper.read(sdcard.getAbsolutePath() + "/accelData/" + "walkRestModel.model");
+            int df = classifier.getNumFolds();
+            df++;
+            //classifier = (J48) (new ObjectInputStream(new FileInputStream(sdcard.getAbsolutePath() + "/accelData/" + "walkRestModel.model"))).readObject();
+            //classifier = (J48) SerializationHelper.read(new FileInputStream(sdcard.getAbsolutePath() + "/accelData/" + "walkRestModel.model"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        };
+
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
@@ -93,26 +133,133 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             started = true;
         }
     }
-
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (started) {
-            double x = sensorEvent.values[0];
-            double y = sensorEvent.values[1];
-            double z = sensorEvent.values[2];
+            if(System.currentTimeMillis() - lastSensorUpdate > 200) {
+                //tvData.setText("No of samples" + counter);
+                double x = sensorEvent.values[0];
+                double y = sensorEvent.values[1];
+                double z = sensorEvent.values[2];
+                counter++;
+
+                double sample = Math.sqrt(Math.pow(x,2) + Math.pow(y,2) + Math.pow(z,2));
+                window1.add(sample);
+                if(counter >= 64) {
+                    window2.add(sample);
+                }
+
+                if(window1.size() == 128) {
+                    calculateWindowData(window1);
+                    window1.clear();
+                }
+                if(window2.size() == 128) {
+                    calculateWindowData(window2);
+                    window2.clear();
+                }
+                lastSensorUpdate = System.currentTimeMillis();
+            }
+
             //long timestamp = System.currentTimeMillis();
-            int typeOfActivity = sItems.getSelectedItemPosition();
+            //int typeOfActivity = sItems.getSelectedItemPosition();
 
             //fs = String.format("X: %d Y: %d Z: %d",x,y,z);
-            String entry = x + "," + y + "," + z + ","  + typeOfActivity + "\n";
-            try {
-                if (outputStreamFlag) {
-                    outputStream.write(entry.getBytes());
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            //String entry = x + "," + y + "," + z + ","  + typeOfActivity + "\n";
+//            try {
+//                if (outputStreamFlag) {
+//                    outputStream.write(entry.getBytes());
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
             //tvData.setText(timestamp + "," + x+"," + y + "," + z);
+        }
+    }
+
+    private void createAttributes(){
+        // Declare two numeric attributes
+        Attribute Attribute1 = new Attribute("min");
+        Attribute Attribute2 = new Attribute("max");
+        Attribute Attribute3 = new Attribute("sd");
+        //Attribute Attribute4 = new Attribute("class");
+
+        // Declare a nominal attribute along with its values
+        FastVector fvNominalVal = new FastVector(2);
+        fvNominalVal.addElement("walking");
+        fvNominalVal.addElement("rest");
+        Attribute Attribute4 = new Attribute("class", fvNominalVal);
+
+        fvWekaAttributes = new FastVector(4);
+        fvWekaAttributes.addElement(Attribute1);
+        fvWekaAttributes.addElement(Attribute2);
+        fvWekaAttributes.addElement(Attribute3);
+        fvWekaAttributes.addElement(Attribute4);
+
+    }
+
+    private void calculateWindowData(ArrayList<Double> window) {
+        Log.d("Window", "Calculated");
+        int N = 128; //window size
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+        double sum = 0;
+        for (int i = 0; i < N; i++) {
+            if(window.get(i)<min)
+            {
+                min = window.get(i);
+            }
+            if(window.get(i)>max)
+            {
+                max = window.get(i);
+            }
+            sum += window.get(i);
+        }
+        double mean = sum/N; //corresponds to 𝑥𝑥̅ in the formula
+        double summedDifference = 0;
+        for (int i = 0; i < N; i++) {
+            summedDifference += Math.pow((window.get(i) - mean),2);
+        }
+        double sd = Math.sqrt(summedDifference / (N-1)); //standard deviation
+
+        int typeOfActivity = sItems.getSelectedItemPosition();
+        String entry = lastSensorUpdate + "," + min + "," + max + "," + sd + ","  + typeOfActivity + "\n";
+        try {
+            if (outputStreamFlag) {
+                outputStream.write(entry.getBytes());
+                counter2++;
+                tvData.setText("No of samples: " + counter2);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Instances data = new Instances("TestInstances",fvWekaAttributes,1);
+        data.setClassIndex(3);
+
+        inst_co = new DenseInstance(data.numAttributes());
+
+        inst_co.setValue((Attribute)fvWekaAttributes.elementAt(1), min);
+        inst_co.setValue((Attribute)fvWekaAttributes.elementAt(2), max);
+        inst_co.setValue((Attribute)fvWekaAttributes.elementAt(3), sd);
+        //inst_co.setMissing((Attribute)fvWekaAttributes.elementAt(4));
+        data.add(inst_co);
+        inst_co.setDataset(data);
+
+
+       /* Instance iExample = new SparseInstance(4);
+        iExample.setValue((Attribute)fvWekaAttributes.elementAt(0), min);
+        iExample.setValue((Attribute)fvWekaAttributes.elementAt(1), max);
+        iExample.setValue((Attribute)fvWekaAttributes.elementAt(2), sd);*/
+        //iExample.setValue((Attribute)fvWekaAttributes.elementAt(3), "positive");
+
+        try {
+            double result = classifier.classifyInstance(inst_co);
+            double[] fDistribution = classifier.distributionForInstance(inst_co);
+            tvData.setText("Prob of activity 1: " + fDistribution[0] + " 2: " + fDistribution[1]);
+            //double ssdd = result +1;
+            //ssdd++;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
