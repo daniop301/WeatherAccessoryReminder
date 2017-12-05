3deca0a51187d85aed37dfe82ec6e8f44f2dd9db
Stefan Bejan <stefan.bejan90@gmail.com>
2017-12-05 11:32:03 +0100

    Added empty aggregator and widgets classes.

---
+++ app/src/main/java/ca/group3/weatheraccessoryreminder/Aggregators/ActivityAggregator.java
@@ -0,0 +1,8 @@
+package ca.group3.weatheraccessoryreminder.Aggregators;
+
+/**
+ * Created by stefa on 12/5/2017.
+ */
+
+public class ActivityAggregator {
+}
---
+++ app/src/main/java/ca/group3/weatheraccessoryreminder/Widgets/WeatherWidget.java
@@ -0,0 +1,8 @@
+package ca.group3.weatheraccessoryreminder.Widgets;
+
+/**
+ * Created by stefa on 12/5/2017.
+ */
+
+public class WeatherWidget {
+}
